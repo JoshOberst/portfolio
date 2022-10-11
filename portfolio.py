c533fc18c7996b38069d7356f4ae1cccc38eeaa0
JoshOberst <joshoberst11@gmail.com>
2022-10-10 20:06:14 -0400

    Update portfolio.py

--- portfolio.py
+++ portfolio.py
@@ -1,3 +1,19 @@
 import numpy as np
 import pandas as pd
-import yahoofinancials as yf
+from yahoofinancials import YahooFinancials
+import yfinance as yf
+import datetime as dt
+import nasdaqdatalink as nl
+
+
+stockList = ['AAPL','IBM','TSLA'] #list of stocks to analyse
+benchmark = ['SPY']                 #benchmark stock ticker
+startTime = dt.datetime(2010,1,1)   #start time of the data
+
+#getting treasury rates
+mydata = nl.get("USTREASURY/BILLRATES")
+print(mydata.index)
+
+#data = yf.download(benchmark+stockList,startTime).get('Adj Close')
+
+#print(data)
